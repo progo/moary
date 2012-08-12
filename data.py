dd09985389dc92fdec134d0395802cbed1ea806c
Mikael Puhakka <mr.progo@gmail.com>
2012-08-12 20:15:36 +0300

    Refactor database facilities.
    Our system here is not among the most performant ones. I think we'll be fine.

--- data.py
+++ data.py
@@ -1,25 +1,31 @@
 """Persistent data facilities.
 We use SQlite 3 to store all these reviews."""
 
-# CREATE TABLE movies
-#      (movie text,
-#       rating real,
-#       imdb text,
-#       message text,
-#       origdate text,
-#       updated text);
-
+import os, sys
 import sqlite3
+
+CREATE = """CREATE TABLE IF NOT EXISTS movies
+            (movie text,
+             rating real,
+             imdb text,
+             message text,
+             origdate text,
+             updated text);"""
+
 DBFILE = "movies.db"
+DATADIR = "data"
+
+def establish_connection():
+    dbfileloc = os.path.join(sys.path[0], DATADIR, DBFILE)
+    con = sqlite3.connect(dbfileloc)
+    con.execute(CREATE)
+    con.commit()
+    return con
 
 def store_entry(e):
     """push an Entry to the database."""
-    with sqlite3.connect(DBFILE) as con:
+    with establish_connection() as con:
         con.execute("INSERT INTO movies VALUES (?,?,?,?,?,?)",
-                (e.movie,
-                 e.rating,
-                 e.imdb,
-                 e.message,
-                 e.origdate,
-                 e.update))
+                (e.movie, e.rating, e.imdb, e.message, e.origdate, e.update))
+        con.commit()
 
