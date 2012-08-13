6c7274f5ca95fa6ce8188bb8daf858d539d536fd
Mikael Puhakka <mr.progo@gmail.com>
2012-08-13 13:56:06 +0300

    First draft at command-line interface.

--- README.rst
+++ README.rst
@@ -4,13 +4,27 @@ Moary
 A work-in-progress effort to provide a local movie rating database for CLI. The
 workflow with the usable product follows:
 
-1. Watch the picture, gather your thoughts about it.
-2. Run ``moary.py``.
-3. ``$EDITOR`` will open with a simple form. Fill as much as you'd like to.
-   Movie name is the only required field. Save and exit.
-4. If you didn't enter IMDB id or URL, Moary queries IMDB with 7 closest names
-   and you choose the one you meant.
-5. There it is now. Saved in database somewhere.
+interactive workflow
+
+    1. Watch the picture, gather your thoughts about it.
+    2. Run ``moary.py add``.
+    3. ``$EDITOR`` will open with a simple form. Fill as much as you'd like to.
+       Movie name is the only required field. Save and exit.
+    4. If you didn't enter IMDB id or URL, Moary queries IMDB with 7 closest
+       names and you choose the one you meant.
+    5. There it is now. Saved in database somewhere.
+
+enter from CLI
+
+    1. Watch the picture, gather your thoughts about it.
+    2. As an example::
+
+        moary.py add "Mulholland Dr." -r 5 -m "Very nice movie."
+
+    3. As you didn't enter IMDB url (with the optional ``-i`` flag), you get
+       the prompt asking about correct IMDB title.
+    4. Override query with the flag ``-I`` (don't query IMDB) or enter
+       correct(ish) IMDB id with ``-i``.
 
 A some sort of list or search capability will appear some time.
 
--- moary.py
+++ moary.py
@@ -1,12 +1,72 @@
 #!/usr/bin/python
 
-from edit_entry import edit_data_interactive
+import argparse
+
+import edit_entry
+from entry import Entry
 import data
 
+
+def do_add(args):
+    """CLI func to call when doing subtask "add". """
+    if not args.movie:
+        try:
+            newflick = edit_entry.edit_data_interactive(None,
+                    skip_imdb=args.skip_imdb)
+        except edit_entry.UserCancel:
+            print "Empty name, exiting..."
+            return 0
+    else:
+        newflick = Entry(unicode(args.movie), args.rating,
+                args.imdb, unicode(args.message))
+        # TODO, warning: WET code
+        clean_id = edit_entry.clean_imdb_id(newflick.imdb)
+        if not clean_id:
+            newflick.imdb = '' if args.skip_imdb \
+                else edit_entry.ask_imdb_interactive(newflick.movie)
+        else:
+            newflick.imdb = clean_id
+    data.store_entry(newflick)
+
+def do_list(args):
+    """CLI func to call when doing subtask "list". """
+    print "listing!"
+
+def _create_and_parse_args():
+    """Create the arg parser and do the magic."""
+    psr = argparse.ArgumentParser(description="A movie diary.")
+    subparser = psr.add_subparsers(
+            title="Subtasks",
+            help=None)
+
+    psr.add_argument('-I', '--skip-imdb', action='store_true',
+            help="Don't query IMDB")
+
+    addparser = subparser.add_parser('add',
+            help="Add new entry")
+    addparser.add_argument("movie",
+            nargs='?',
+            help="Movie name")
+    addparser.add_argument("-r", "--rating", type=float,
+            help="How did you like the movie?")
+    addparser.add_argument("-i", "--imdb",
+            help="IMDB id or URL")
+    addparser.add_argument("-m", "--message",
+            help="A few words about the experience.")
+    addparser.set_defaults(func=do_add)
+
+    listparser = subparser.add_parser('list',
+            help='List entries')
+    listparser.set_defaults(func=do_list)
+    # TODO: to be concluded
+
+    args = psr.parse_args()
+    return args
+
 if __name__ == '__main__':
+    """parse args and direct execution towards the right func."""
     import sys
     reload(sys)
     sys.setdefaultencoding('utf-8') # wtf
-    newflick = edit_data_interactive(None)
-    print newflick.__dict__
-    data.store_entry(newflick)
+    args = _create_and_parse_args()
+    args.func(args)
