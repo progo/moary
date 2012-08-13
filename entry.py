b4be909dfbb5f48576c48859f5e0eda8e7daaf0f
Mikael Puhakka <mr.progo@gmail.com>
2012-08-13 13:21:03 +0300

    Set defaults

--- entry.py
+++ entry.py
@@ -5,11 +5,11 @@ class Entry():
     def __init__(self, movie, rating=0, imdb='', message='',
             origdate='', update=''):
         self.movie = movie
-        self.rating = rating
-        self.imdb = imdb
-        self.message = message
-        self.origdate = origdate
-        self.update = update
+        self.rating = rating or 0
+        self.imdb = imdb or ''
+        self.message = message or ''
+        self.origdate = origdate or ''
+        self.update = update or ''
 
     def __repr__(self):
         return "{0} ({1})".format(self.movie, self.rating)
