9b00ab464b539acf9289bbd6a77871ce19d00b6f
Mikael Puhakka <mr.progo@gmail.com>
2012-08-21 11:44:21 +0300

    All kinds of stuff to tests.

--- tests.py
+++ tests.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # Too late, too late.
+import os
 import unittest
 from ludibrio import Stub, Mock, Dummy
 from io import BytesIO
 
 from entry import Entry
+import moary
 import edit_entry, data
 
 class FileMockReadonly(BytesIO):
@@ -18,6 +20,35 @@ class FileMock(BytesIO):
     name = ''
 
 class MoaryTestCase(unittest.TestCase): pass
+
+### Test batch adds, black box baby. (Not exactly BB but closer...)
+
+class MoaryBatchAddTestCase(MoaryTestCase):
+    TESTDB = "movies.db_tests"
+    COMMON_ARGS = ["-f", TESTDB]
+
+    def setUp(self):
+        try:
+            os.remove(self.TESTDB)
+        except OSError:
+            pass # file doesn't exist
+
+    def tearDown(self):
+        os.remove(self.TESTDB)
+
+    def call(self, cmdline):
+        moary.main(self.COMMON_ARGS + cmdline.split())
+
+class TestGoodAddMovieOnly(MoaryBatchAddTestCase):
+    """test adding only the movie name."""
+    def testAdd(self):
+        self.call("add ABC")
+        # actually, should call ask_imdb_interactive but doesn't.
+        entry = data.DataFacilities(dbfile=self.TESTDB).get_last()
+        self.assertEquals('ABC', entry.movie)
+
+class TestGoodAddIMDBonly(MoaryBatchAddTestCase):
+    pass
 
 ### Adding from nothing
 
@@ -54,16 +85,47 @@ class TestGoodAdd(MoaryAddTestCase):
         entry = edit_entry.edit_data_interactive({}, skip_imdb=True)
         self.assertEquals(entry.movie, "ABC")
         self.assertEquals(entry.rating, '4')
+        self.assertEquals(entry.message, "Cool movie.")
+        self.assertEquals(entry.imdb, '01234')
+
+class TestAddNoIMDB(MoaryAddTestCase):
+    """test a situation no IMDB id has been provided."""
+
+    filecontents = """Movie: def
+    Rating: 4
+    IMDB: 
+    ----- Review -----
+    Cool movie.
+    """
+ 
+    def testSimpleAdd(self):
+        entry = edit_entry.edit_data_interactive({}, skip_imdb=True)
+        self.assertEquals(entry.movie, "def")
+        self.assertEquals(entry.rating, '4')
+        self.assertEquals(entry.imdb, '')
+        self.assertEquals(entry.message, "Cool movie.")
+
+class TestAddFaultyIMDB(MoaryAddTestCase):
+    """test a situation a faulty IMDB id has been provided."""
+
+    filecontents = """Movie: def
+    Rating: 4
+    IMDB: vccv
+    ----- Review -----
+    Cool movie.
+    """
+ 
+    def testSimpleAdd(self):
+        entry = edit_entry.edit_data_interactive({}, skip_imdb=True)
+        self.assertEquals(entry.movie, "def")
+        self.assertEquals(entry.rating, '4')
+        self.assertEquals(entry.imdb, '')
+        self.assertEquals(entry.message, "Cool movie.")
 
 class TestEmptyAdd(MoaryAddTestCase):
     """test a situation where the user provides nothing new. Should raise
     UserCancel."""
-    filecontents = """Movie: 
-    Rating: 
-    IMDB:  
-    ----- Review -----
-    """
-
+    filecontents = edit_entry.fill_in_form(None)
     def testEmptyAdd(self):
         self.assertRaises(edit_entry.UserCancel,
             lambda: edit_entry.edit_data_interactive({}, skip_imdb=True))
